9dddf0a5af406d3c2d62a54f3be297859bfdf0c9
n-devs <109496593+natthp49@users.noreply.github.com>
2023-03-04 14:05:19 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,9 @@
-const path = require("path");
 
-const server = path.join(__dirname, "src/server.js");
 module.exports = {
 	apps: [
 		{
 			name: "app1",
-			script: server,
+			script: "./src/server.js",
 		},
 	],
 };
